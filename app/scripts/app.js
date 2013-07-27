73e72be9fb84c35b49e4cb753699d43ea620d897
Pablo De Nadai <pablodenadai@gmail.com>
2013-07-28 01:59:50 +1000

    Install angular-seed via yeoman

---
+++ app/scripts/app.js
@@ -0,0 +1,13 @@
+'use strict';
+
+angular.module('NodeAngularJSHTML5modeApp', [])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
